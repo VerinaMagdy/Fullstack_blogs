43b2bff83dd32d21968856529242e0b581e9fe16
Verina Magdy <verina.magdy@gmail.com>
2021-09-22 20:57:51 +0200

    lastt commit

--- app.js
+++ app.js
@@ -1,107 +1,64 @@
-// //express more easier and organized than server
-// const express = require('express');
-// const morgan = require('morgan');//3rd party
-// const mongoose = require('mongoose'); 
-// const Blog = require('./models/blog');
-// const blogRoutes = require('./routes/blogRoutes');
-// const translateRoutes = require('./routes/translateRoutes');
-
-
-// // express app
-// const app = express();
-
-// //connect to mongodb
-// const dbURI= 'mongodb+srv://netninja:test1234@nodetuts.5l1cz.mongodb.net/nodetuts?retryWrites=true&w=majority';
-// mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
-// .then((result)=>app.listen(process.env.PORT||3000))
-//   .catch(err => console.log(err)); 
-
- 
-// // register view engine
-// app.set('view engine', 'ejs');
-// // app.set('views', 'myviews');//name of new folder myviews
-
-// // middleware & static files(like css)
-// app.use(express.static('public'));//public is a folder name
-// app.use(express.urlencoded({ extended: true }));
-// app.use(morgan('dev'));
-
-// app.use((req, res, next) => {
-//   res.locals.path = req.path;
-//   next();
-// });
-
-
-// //routes
-// app.get('/', (req, res) => {
-//    //res.send('<p>home page</p>');
-//   //res.sendFile('./views/index.html', { root: __dirname });
-//   res.redirect('/blogs');
-// });
-
-// app.get('/about', (req, res) => {
-//   // res.send('<p>about page</p>');
-//  // res.sendFile('./views/about.html', { root: __dirname });
-//  res.render('about', { title: 'About' });
-// });
-
-// // redirects
-// // app.get('/about-us', (req, res) => {
-// //   res.redirect('/about');
-// // });
-
-
-// //blog routes
-// app.use('/blogs',blogRoutes)
-
-// //new route
-// app.use('/translate',translateRoutes)
-
-// // 404 page
-// app.use((req, res) => {
-//   res.status(404).render('404', { title: '404' });
-// });
-
-
+//express more easier and organized than server
 const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require('morgan');//3rd party
+const mongoose = require('mongoose'); 
+const Blog = require('./models/blog');
 const blogRoutes = require('./routes/blogRoutes');
+const translateRoutes = require('./routes/translateRoutes');
+
 
 // express app
 const app = express();
 
-// connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+//connect to mongodb
+const dbURI= 'mongodb+srv://netninja:test1234@nodetuts.5l1cz.mongodb.net/nodetuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then((result)=>app.listen(process.env.PORT||3000))
+  .catch(err => console.log(err)); 
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
-  
+ 
 // register view engine
 app.set('view engine', 'ejs');
-// middleware & static files
-app.use(express.static('public'));
+// app.set('views', 'myviews');//name of new folder myviews
+
+// middleware & static files(like css)
+app.use(express.static('public'));//public is a folder name
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-// routes
+
+//routes
 app.get('/', (req, res) => {
+   //res.send('<p>home page</p>');
+  //res.sendFile('./views/index.html', { root: __dirname });
   res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+  // res.send('<p>about page</p>');
+ // res.sendFile('./views/about.html', { root: __dirname });
+ res.render('about', { title: 'About' });
 });
 
-// blog routes
-app.use('/blogs', blogRoutes);
+// redirects
+// app.get('/about-us', (req, res) => {
+//   res.redirect('/about');
+// });
+
+
+//blog routes
+app.use('/blogs',blogRoutes)
+
+//new route
+app.use('/translate',translateRoutes)
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
